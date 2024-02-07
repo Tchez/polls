0ade8c6f81fb63a06bc208a1897d40e192716cc3
Marco Antônio Martins Porto Netto <marco.netto@mjv.net>
2024-02-07 15:25:37 -0300

    feat: :sparkles: adicionando endpoint para votação

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,19 @@
 import { fastify } from "fastify";
 import { z } from "zod";
 import { createPoll } from "./routes/crate-poll";
+import { getPoll } from "./routes/get-poll";
+import { voteOnPoll } from "./routes/vote-on-poll";
+import cookie from "@fastify/cookie";
 
 const app = fastify();
 
+app.register(cookie, {
+  secret: "polls-app-secret",
+  hook: "onRequest",
+});
 app.register(createPoll);
+app.register(getPoll);
+app.register(voteOnPoll);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server started at http://localhost:3333");
