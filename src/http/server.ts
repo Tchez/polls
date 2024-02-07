1231a6135420f3d8ce5c646db13e05e78687ae41
Marco Antônio Martins Porto Netto <marco.netto@mjv.net>
2024-02-07 13:15:53 -0300

    feat: :sparkles: setup configs + docker-compose

---
+++ src/http/server.ts
@@ -0,0 +1,11 @@
+import { fastify } from 'fastify';
+
+const app = fastify();
+
+app.get('/hello', async (request, reply) => {
+    return { hello: 'world' };
+});
+
+app.listen({ port: 3333 }).then(() => {
+    console.log('Server started at http://localhost:3333');
+}); 
