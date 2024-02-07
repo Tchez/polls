edc1ca10b395392d6e3dfdd758947d56b68134fc
Marco Antônio Martins Porto Netto <marco.netto@mjv.net>
2024-02-07 13:46:18 -0300

    feat: :sparkles: implementação inicial das rotas de criação de polls

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,21 @@
-import { fastify } from 'fastify';
+import { fastify } from "fastify";
+import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
 
 const app = fastify();
 
-app.get('/hello', async (request, reply) => {
-    return { hello: 'world' };
+const prisma = new PrismaClient();
+
+app.post("/polls", async (req, reply) => {
+  const createPollBody = z.object({
+    title: z.string(),
+  });
+
+  const { title } = createPollBody.parse(req.body);
+  const poll = await prisma.poll.create({ data: { title } });
+  return reply.code(201).send({ poll_id: poll.id });
 });
 
 app.listen({ port: 3333 }).then(() => {
-    console.log('Server started at http://localhost:3333');
-}); 
+  console.log("Server started at http://localhost:3333");
+});
